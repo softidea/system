3e33a7d10f34196dca01a5fa75ba7fc416025dec
dinet <dinet@DESKTOP-S3O8Q1H>
2016-07-09 21:19:32 +0530

    creat customer pagenization

--- customer/customer_view.php
+++ customer/customer_view.php
@@ -14,11 +14,26 @@
         <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,700,600italic,700italic,900,900italic' rel='stylesheet' type='text/css'>
         <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />
         <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+        
+        <?php if (isset($_GET['bootstrap']) && $_GET['bootstrap'] == 1): ?>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <?php else: ?>
+        <link rel="stylesheet" type="text/css" href="../assets/css/zebra_pagination.css">
+        <?php endif ?>
+        
         <link rel="stylesheet" type="text/css" href="../assets/css/customer_registration.css">
         <link rel="icon" href="favicon.ico">
     </head>
     <body>
-        <?php include '../assets/include/navigation_bar.php'; ?>
+        <?php 
+        include '../assets/include/navigation_bar.php';
+        
+         $conn = mysqli_connect("77.104.142.97", "ayolanin_dev", "WelComeDB1129", "ayolanin_datahost");
+        if (mysqli_connect_errno()) {
+        echo "Falied to Connect the Database" . mysqli_connect_error();
+        }
+        
+        ?>
         <!--Customer Panel Section-->
         <div class="container" style="margin-top: 80px;display: block;" id="one">
             <div class="row">
@@ -63,6 +78,25 @@
                                 </div>
                             </fieldset>
 
+                             <!--pagination for Customer View-->
+                            <?php
+                            global $conn;
+                            $records_per_page = 10;
+                            require 'Zebra_Pagination.php';
+                            $pagination = new Zebra_Pagination();
+
+                            $sql_query = "SELECT a.`cus_fullname`,a.`cus_nic`,a.`cus_address`,a.`cus_reg_date`,a.`cus_tp`,count(b.`ser_number`) as service_details,b.`ser_number` from `customer` a inner join `service` b on a.`cus_nic`=b.`cus_nic` LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                            $result = mysqli_query($conn, $sql_query);
+                            if (!($result)) {
+
+                            // stop execution and display error message
+                            die(mysql_error());
+                            }
+                            $rows = mysqli_fetch_assoc(mysqli_query($conn, 'SELECT FOUND_ROWS() AS rows'));
+                            $pagination->records($rows['rows']);
+                            $pagination->records_per_page($records_per_page);
+                            ?>
+                            
                             <div class="col-sm-12">
                                 <table class="table table-bordered table-hover">
                                     <thead>
@@ -78,53 +112,27 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Malinda Prabhash Senanayake</td>
-                                            <td>931781258V</td>
-                                            <td>Kandy</td>
-                                            <td>2016/06/25</td>
-                                            <td>0774585256</td>
-                                            <td>2</td>
-                                            <td><a href="customer_updateinfo.php">HOR-1258</a></td>
+                                       <?php $index = 0; ?>
+                                                <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                                                <tr<?php echo $index++% 2 ? ' class="even"' : '' ?>>
 
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Malinda Prabhash Senanayake</td>
-                                            <td>931781258V</td>
-                                            <td>Kandy</td>
-                                            <td>2016/06/25</td>
-                                            <td>0774585256</td>
-                                            <td>2</td>
-                                            <td><a href="customer_updateinfo.php">HOR-1258</a></td>
+                                                    <td><?php echo' 1' ?></td>
+                                                    <td><?php echo $row['cus_fullname'] ?></td>
+                                                    <td><?php echo $row['cus_nic'] ?></td>
+                                                    <td><?php echo $row['cus_address'] ?></td>
+                                                    <td><?php echo $row['cus_reg_date'] ?></td>
+                                                    <td><?php echo $row['cus_tp'] ?></td>
+                                                    <td><?php echo $row['service_details'] ?></td>
+                                                    <td><?php echo $row['ser_number'] ?></td>
 
-                                        </tr>
+                                                </tr>
+                                                <?php endwhile ?>
                                     </tbody>
                                 </table>
-                                <div style="text-align: center;">
-                                    <nav>
-                                        <ul class="pagination">
-                                            <li class="page-item">
-                                                <a class="page-link" href="#" aria-label="Previous">
-                                                    <span aria-hidden="true">&laquo;</span>
-                                                    <span class="sr-only">Previous</span>
-                                                </a>
-                                            </li>
-                                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">4</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">5</a></li>
-                                            <li class="page-item">
-                                                <a class="page-link" href="#" aria-label="Next">
-                                                    <span aria-hidden="true">&raquo;</span>
-                                                    <span class="sr-only">Next</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                </div>
+                                 <div class="text-center">
+                                            <nav> <ul class="pagination"><li> <?php $pagination->render(); ?></li></ul></nav>
+                                        </div>
+                                
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <div class="form-inline">
