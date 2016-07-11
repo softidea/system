0d438f326440e26fc0f37d32a01be6c7b68909c7
dinet <dinet@DESKTOP-S3O8Q1H>
2016-07-11 10:42:16 +0530

    creat customer pagenization for view

--- customer/customer_view.php
+++ customer/customer_view.php
@@ -14,25 +14,24 @@
         <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,700,600italic,700italic,900,900italic' rel='stylesheet' type='text/css'>
         <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />
         <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-        
+
         <?php if (isset($_GET['bootstrap']) && $_GET['bootstrap'] == 1): ?>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <?php else: ?>
-        <link rel="stylesheet" type="text/css" href="../assets/css/zebra_pagination.css">
+            <link rel="stylesheet" type="text/css" href="../assets/css/zebra_pagination.css">
         <?php endif ?>
-        
+
         <link rel="stylesheet" type="text/css" href="../assets/css/customer_registration.css">
         <link rel="icon" href="favicon.ico">
     </head>
     <body>
-        <?php 
+        <?php
         include '../assets/include/navigation_bar.php';
-        
-         $conn = mysqli_connect("77.104.142.97", "ayolanin_dev", "WelComeDB1129", "ayolanin_datahost");
+
+        $conn = mysqli_connect("77.104.142.97", "ayolanin_dev", "WelComeDB1129", "ayolanin_datahost");
         if (mysqli_connect_errno()) {
-        echo "Falied to Connect the Database" . mysqli_connect_error();
+            echo "Falied to Connect the Database" . mysqli_connect_error();
         }
-        
         ?>
         <!--Customer Panel Section-->
         <div class="container" style="margin-top: 80px;display: block;" id="one">
@@ -48,55 +47,90 @@
                                     <legend>Search Option 01</legend>
                                     <div class="form-group required">
                                         <label class="control-label" for="input-email">Search by:</label>
-                                        <select name="cbopayment" id="input-search-option" class="form-control" required onchange="setServiceOptionPanel();">
-                                            <option value=""> --- Please Select --- </option>
-                                            <option value="cname">Customer NIC</option>
-                                            <option value="tp">Phone Number</option>
-                                            <option value="serviceno">Service Number</option>
-                                        </select>
+                                        <form method="post" >  <select name="cbopayment" id="input-search-option" class="form-control" required onchange="setServiceOptionPanel();">
+                                                <option value=""> --- Please Select --- </option>
+                                                <option value="nic">Customer NIC</option>
+                                                <option value="tp">Phone Number</option>
+                                                <option value="sno">Service Number</option>
+                                            </select>
+                                            <div class="form-group required">
+                                                <label class="control-label" for="input-email">Search Here:</label>
+                                                <input type="text" name="fname" id="fname" value="" placeholder="Search Here" id="input-email" class="form-control" maxlength="10" required/>
+                                                <br>
+                                                <button type="submit" name="search_buton"  id="cservicebtn" method="post" class="btn btn">Search</button>
+                                            </div>
+                                        </form>
+                                        <?php
+                                        if (isset($_POST['cbopayment'])) {
+                                            $com_vehi = $_POST['cbopayment'];
+                                           
+                                        }
+                                        ?>
                                     </div>
-                                    <div class="form-group required">
-                                        <label class="control-label" for="input-email">Search Here:</label>
-                                        <input type="text" name="fname" id="fname" value="" placeholder="Search Here" id="input-email" class="form-control" required/>
-                                        <br>
-                                        <button type="button" id="cservicebtn" class="btn btn">Search</button>
-                                    </div>
+
                                 </fieldset>
                             </div>
 
                             <fieldset id="account">
                                 <legend>Search Option 02</legend>
+                               <form method="post">
                                 <div class="form-group required">
                                     <label class="control-label" for="input-email">Start Date:</label>
-                                    <input type="date" name="fname" id="fname" value="" placeholder="Registration Date" id="input-email" class="form-control"/>
+                                    <input type="date" name="date1" id="fname" value="" placeholder="Registration Date" id="input-email" class="form-control"/>
                                 </div>
                                 <div class="form-group required">
                                     <label class="control-label" for="input-email">End Here:</label>
-                                    <input type="date" name="fname" id="fname" value="" placeholder="Search Here" id="input-email" class="form-control" required/>
+                                    <input type="date" name="date2" id="fname" value="" placeholder="Search Here" id="input-email" class="form-control" required/>
                                     <br>
-                                    <button type="button" id="cservicebtn" class="btn btn">Search</button>
+                                     <button type="submit" on name="search_date" id="cservicebtn" class="btn btn">Search</button>
                                 </div>
+                               </form>
                             </fieldset>
 
-                             <!--pagination for Customer View-->
+
+                            <!--pagination for Customer View-->
                             <?php
                             global $conn;
+                            $sql_query = "";
+
                             $records_per_page = 10;
                             require 'Zebra_Pagination.php';
                             $pagination = new Zebra_Pagination();
-
-                            $sql_query = "SELECT a.`cus_fullname`,a.`cus_nic`,a.`cus_address`,a.`cus_reg_date`,a.`cus_tp`,count(b.`ser_number`) as service_details,b.`ser_number` from `customer` a inner join `service` b on a.`cus_nic`=b.`cus_nic` LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                            if (isset($_POST['search_buton'])) {
+                                if ($com_vehi=="sno") {
+                                   $sql_query = "SELECT SQL_CALC_FOUND_ROWS a.cus_id,a.cus_fullname,a.cus_nic,a.cus_address,a.cus_reg_date,a.cus_tp,b.ger_fullname,b.ger_nic FROM customer a LEFT JOIN ger b ON a.cus_nic=b.cus_nic INNER JOIN `service` c ON a.`cus_nic`=c.`cus_nic` ORDER BY a.cus_id WHERE c.`ser_number`='".$_POST['fname']."' LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                                }
+                                elseif ($com_vehi=="tp") {
+                                $sql_query = "SELECT SQL_CALC_FOUND_ROWS a.cus_id,a.cus_fullname,a.cus_nic,a.cus_address,a.cus_reg_date,a.cus_tp,b.ger_fullname,b.ger_nic FROM customer a LEFT JOIN ger b ON a.cus_nic=b.cus_nic ORDER BY a.cus_id WHERE a.`cus_tp`='".$_POST['fname']."' LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                            }
+                                elseif ($com_vehi=="nic") {
+                                $sql_query = "SELECT SQL_CALC_FOUND_ROWS a.cus_id,a.cus_fullname,a.cus_nic,a.cus_address,a.cus_reg_date,a.cus_tp,b.ger_fullname,b.ger_nic FROM customer a LEFT JOIN ger b ON a.cus_nic=b.cus_nic ORDER BY a.cus_id WHERE a.`cus_nic`='".$_POST['fname']."' LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                            }
+                               
+                            } 
+                            
+                            elseif (isset($_POST['search_date'])) {
+                              
+                                $sql_query = "SELECT SQL_CALC_FOUND_ROWS a.cus_id,a.cus_fullname,a.cus_nic,a.cus_address,a.cus_reg_date,a.cus_tp,b.ger_fullname,b.ger_nic FROM customer a LEFT JOIN ger b ON a.cus_nic=b.cus_nic ORDER BY a.cus_id WHERE a.`cus_reg_date` BETWEEN ".$_POST['date1']." AND ".$_POST['date2']." LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                                
+                            }
+                            
+                            else {
+                                $sql_query = "SELECT SQL_CALC_FOUND_ROWS a.cus_id,a.cus_fullname,a.cus_nic,a.cus_address,a.cus_reg_date,a.cus_tp,b.ger_fullname,b.ger_nic FROM customer a LEFT JOIN ger b ON a.cus_nic=b.cus_nic ORDER BY a.cus_id LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . "," . $records_per_page;
+                               
+                            }
                             $result = mysqli_query($conn, $sql_query);
+                             $service_co=mysqli_num_rows($result);
                             if (!($result)) {
 
-                            // stop execution and display error message
-                            die(mysql_error());
+                                // stop execution and display error message
+                                die(mysql_error());
                             }
                             $rows = mysqli_fetch_assoc(mysqli_query($conn, 'SELECT FOUND_ROWS() AS rows'));
                             $pagination->records($rows['rows']);
                             $pagination->records_per_page($records_per_page);
                             ?>
-                            
+
                             <div class="col-sm-12">
                                 <table class="table table-bordered table-hover">
                                     <thead>
@@ -107,32 +141,39 @@
                                             <th>Permanent Address</th>
                                             <th>Registration Date</th>
                                             <th>Phone Number</th>
-                                            <th>Service Details</th>
-                                            <th>Update Action</th>
+                                            <th>Guarantor Name</th>
+                                            <th>Guarantor NIC</th>
+                                            <th>View More</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                       <?php $index = 0; ?>
-                                                <?php while ($row = mysqli_fetch_assoc($result)): ?>
-                                                <tr<?php echo $index++% 2 ? ' class="even"' : '' ?>>
+                                        <?php
+                                        $index = 0;
+                                        $i = 1;
+                                        
+                                        ?>
+                                        <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                                            <tr<?php echo $index++ % 2 ? ' class="even"' : '' ?>>
 
-                                                    <td><?php echo' 1' ?></td>
-                                                    <td><?php echo $row['cus_fullname'] ?></td>
-                                                    <td><?php echo $row['cus_nic'] ?></td>
-                                                    <td><?php echo $row['cus_address'] ?></td>
-                                                    <td><?php echo $row['cus_reg_date'] ?></td>
-                                                    <td><?php echo $row['cus_tp'] ?></td>
-                                                    <td><?php echo $row['service_details'] ?></td>
-                                                    <td><?php echo $row['ser_number'] ?></td>
+                                                <td><?php echo $row['cus_id'] ?></td>
+                                                <td><?php echo $row['cus_fullname'] ?></td>
+                                                <td><?php echo $row['cus_nic'] ?></td>
+                                                <td><?php echo $row['cus_address'] ?></td>
+                                                <td><?php echo $row['cus_reg_date'] ?></td>
+                                                <td><?php echo $row['cus_tp'] ?></td>
+                                                <td><?php echo $row['ger_fullname'] ?></td>
+                                                <td><?php echo $row['ger_nic'] ?></td>
+                                                <td><?php echo '<form method="post" action=#><button type="submit" name="view"  id="cservicebtn" method="post" class="btn btn">View</button></form>' ?></td>
 
-                                                </tr>
-                                                <?php endwhile ?>
+                                            </tr>
+                                            <?php $i++; ?>
+                                        <?php endwhile ?>
                                     </tbody>
                                 </table>
-                                 <div class="text-center">
-                                            <nav> <ul class="pagination"><li> <?php $pagination->render(); ?></li></ul></nav>
-                                        </div>
-                                
+                                <div class="text-center">
+                                    <nav> <ul class="pagination"><li> <?php $pagination->render(); ?></li></ul></nav>
+                                </div>
+
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <div class="form-inline">
